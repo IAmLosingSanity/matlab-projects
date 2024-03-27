73b76b5752128d87710960241873107ce5c7ea00
IAmLosingSanity <trufanovila59@gmail.com>
2024-03-27 12:23:10 +0300

    refactoring lab 6

--- lab6.m
+++ lab6.m
@@ -1,33 +1,38 @@
 %% Lab 6. Var 3
 classdef lab6 < handle
     properties
-        docks(1,1) = true % initialize all docks as free
+        docks % логический массив, представляющий доки
     end
-
+    
     methods
+        % Конструктор
         function obj = lab6(size)
-            obj.docks = ones(size, 1); % initialize all docks as free
+            obj.docks = true(1, size); % Инициализация всех доков как свободных
         end
-
-        function response = requestLanding(obj, dockNumber)
-            if dockNumber > size(obj.docks, 1) || dockNumber <= 0
-                response = false; % request is denied
-            elseif ~obj.docks(dockNumber)
-                response = false; % request is denied
+        
+        % Метод для запроса посадки
+        function [permission] = requestLanding(obj, dockNumber)
+            % Проверка существования дока и его состояния
+            if dockNumber > length(obj.docks) || dockNumber < 1
+                permission = false; % Док не существует
+            elseif obj.docks(dockNumber) == false
+                permission = false; % Док занят
             else
-                obj.docks(dockNumber) = false; % mark the dock as occupied
-                response = true; % request is approved
+                obj.docks(dockNumber) = false; % Занять док
+                permission = true; % Разрешение на посадку
             end
         end
-
-        function response = requestTakeoff(obj, dockNumber)
-            if dockNumber > size(obj.docks, 1) || dockNumber <= 0
-                response = false; % request is denied
-            elseif obj.docks(dockNumber)
-                response = false; % request is denied
+        
+        % Метод для запроса взлёта
+        function [permission] = requestTakeoff(obj, dockNumber)
+            % Проверка существования дока и его состояния
+            if dockNumber > length(obj.docks) || dockNumber < 1
+                permission = false; % Док не существует
+            elseif obj.docks(dockNumber) == true
+                permission = false; % Док свободен
             else
-                obj.docks(dockNumber) = true; % mark the dock as free
-                response = true; % request is approved
+                obj.docks(dockNumber) = true; % Освободить док
+                permission = true; % Разрешение на взлёт
             end
         end
     end
--- lab6e.m
+++ lab6e.m
@@ -1,18 +1,31 @@
 % Create a SpacePort object with 5 docks
-spacePort = lab6(5);
+% Создание объекта SpacePort с 5 доками
+mySpacePort = lab6(5);
 
-% Request landing at dock 3
-landingResponse = spacePort.requestLanding(3);
-if landingResponse
-    disp('Landing approved at dock 3');
-else
-    disp('Landing denied at dock 3');
-end
+% Печать текущего статуса доков
+disp('Initial dock status:');
+disp(mySpacePort.docks);
 
-% Request takeoff from dock 3
-takeoffResponse = spacePort.requestTakeoff(3);
-if takeoffResponse
-    disp('Takeoff approved from dock 3');
-else
-    disp('Takeoff denied from dock 3');
-end
+% Попытка запросить посадку в док номер 3
+landingPermission = mySpacePort.requestLanding(3);
+disp(['Request landing at dock 3: ', num2str(landingPermission)]);
+
+% Печать статуса доков после запроса на посадку
+disp('Dock status after landing request:');
+disp(mySpacePort.docks);
+
+% Попытка запросить взлёт из дока номер 3
+takeoffPermission = mySpacePort.requestTakeoff(3);
+disp(['Request takeoff from dock 3: ', num2str(takeoffPermission)]);
+
+% Печать статуса доков после запроса на взлёт
+disp('Dock status after takeoff request:');
+disp(mySpacePort.docks);
+
+% Попытка запросить посадку в несуществующий док номер 6
+landingPermission = mySpacePort.requestLanding(6);
+disp(['Request landing at dock 6: ', num2str(landingPermission)]);
+
+% Попытка запросить взлёт из свободного дока номер 2
+takeoffPermission = mySpacePort.requestTakeoff(2);
+disp(['Request takeoff from dock 2: ', num2str(takeoffPermission)]);
